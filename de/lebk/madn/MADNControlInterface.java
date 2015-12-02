d210ac162784d11e9b87ee9feeed7ec9e8e663e7
Marius Timmer <Marius.Timmer@web.de>
2015-12-02 11:49:16 +0100

    Method added for getting a player by its ID

--- de/lebk/madn/MADNControlInterface.java
+++ de/lebk/madn/MADNControlInterface.java
@@ -1,10 +1,12 @@
 package de.lebk.madn;
 
 import de.lebk.madn.model.Figur;
+import de.lebk.madn.model.Player;
 
 public interface MADNControlInterface {
 
     public boolean moveFigur(Coordinate position, Figur figur, int steps);
     public void userDice();
+    public Player getPlayer(int id);
 
 }
