920d386166200e967f2759eabba64b052c01544a
Marius Timmer <Marius.Timmer@web.de>
2015-12-02 11:09:56 +0100

    Added methods for storing an alternative element

--- de/lebk/madn/gui/BoardElementWaypoint.java
+++ de/lebk/madn/gui/BoardElementWaypoint.java
@@ -6,8 +6,47 @@ import java.awt.Graphics;
 
 public class BoardElementWaypoint extends BoardElement {
 
+    protected Coordinate alternative = null;
+
+    /**
+     * Generates a Waypoint with alternative
+     * @param board The board on which the element will be placed
+     * @param position The position of the element
+     * @param nextElement The coordinate of the following element
+     * @param alternative The coordinate of the laternative following element
+     */
+    public BoardElementWaypoint(Board board, Coordinate position, Coordinate nextElement, Coordinate alternative)
+    {
+        this(board, position, nextElement);
+        this.alternative = alternative;
+    }
+
+    /**
+     * Generates a Waypoint with alternative
+     * @param board The board on which the element will be placed
+     * @param position The position of the element
+     * @param nextElement The coordinate of the following element
+     */
     public BoardElementWaypoint(Board board, Coordinate position, Coordinate nextElement) {
         super(board, position, nextElement);
+    }
+
+    /**
+     * Sets an alternative following point
+     * @param c alternative
+     */
+    public void setAlternative(Coordinate c)
+    {
+	this.alternative = c;
+    }
+
+    /**
+     * Checks if this element has an alternative following
+     * @return True if there is an alternative or false
+     */
+    public boolean hasAlternative()
+    {
+	return (this.alternative != null);
     }
     
     @Override
